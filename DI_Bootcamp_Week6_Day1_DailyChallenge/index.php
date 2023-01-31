0bb336440c13f3066ba008a65085c9b77aaf9292
COULIBALY Drissa Michael <119611293+mikita00@users.noreply.github.com>
2023-01-31 19:54:21 +0100

    First commit

---
+++ DI_Bootcamp_Week6_Day1_DailyChallenge/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DailyChallenge</title>
+  </head>
+  <body>
+      <h1 style="color:red; text-align:center;"> THIS IS ALL PHP'S INFO</h1>
+      <br>
+     <?php
+     phpinfo()
+     ?>
+
+  </body>
+</html>
